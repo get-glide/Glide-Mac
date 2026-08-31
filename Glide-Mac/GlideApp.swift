f06fe420854559f077c6f8ecebc375834acaf947
Pranay Venkat Aluri <145622254+helloGithub326@users.noreply.github.com>
2026-08-30 22:22:15 -0500

    The midnight rollover feature I finished earlier

--- Glide-Mac/GlideApp.swift
+++ Glide-Mac/GlideApp.swift
@@ -1,18 +1,28 @@
 //
-//  Glide_MacApp.swift
+//  GlideApp.swift
 //  Glide-Mac
 //
 //  Created by Aarnav on 6/24/26.
 //
 
 import SwiftUI
-import SwiftData
 
 @main
-struct Glide_MacApp: App {
+struct GlideApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var rolloverManager: RolloverManager?
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        rolloverManager = RolloverManager()
+        rolloverManager?.setup()
     }
 }
